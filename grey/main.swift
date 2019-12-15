f83953cb0f9cc3776c885631700e2468a748ed58
Hasan Gondal <uh@hasan.wtf>
2019-12-15 01:54:01 +0000

    Initial Commit

---
+++ grey/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  grey
+//
+//  Created by Hasan Gondal on 15/12/2019.
+//  Copyright © 2019 Hasan Gondal. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
